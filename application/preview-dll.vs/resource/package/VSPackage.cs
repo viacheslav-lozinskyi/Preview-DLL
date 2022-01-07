297c42e41ca417c7364b3674a9e3f9679dafcb2a
Viacheslav Lozinskyi <viacheslav.lozinskyi@gmail.com>
2022-01-07 14:33:15 +0200

    - Bug with warning message on start of Visual Studio 2022 is fixed

--- application/preview-dll.vs/resource/package/VSPackage.cs
+++ application/preview-dll.vs/resource/package/VSPackage.cs
@@ -1,6 +1,7 @@
 ﻿
+using EnvDTE;
+using EnvDTE80;
 using Microsoft.VisualStudio;
-using Microsoft.VisualStudio.Imaging;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 using System;
@@ -23,83 +24,7 @@ namespace resource.package
             public const string DESCRIPTION = "Quick preview of DLL, EXE and SYS files";
             public const string GUID = "6171BB93-BA14-4ECF-B72B-5107D12E5D0F";
             public const string NAME = "Preview-DLL";
-            public const string VERSION = "1.0.8";
-        }
-
-        internal class InfoBarService : IVsInfoBarUIEvents
-        {
-            private static uint s_Cookie = 0;
-
-            public static void Validate()
-            {
-                try
-                {
-                    ThreadHelper.ThrowIfNotOnUIThread();
-                    if (string.IsNullOrEmpty(atom.Trace.GetFailState(CONSTANT.APPLICATION)) == false)
-                    {
-                        var a_Context1 = Package.GetGlobalService(typeof(SVsInfoBarUIFactory)) as IVsInfoBarUIFactory;
-                        var a_Context2 = (IVsInfoBarHost)null;
-                        if (a_Context1 != null)
-                        {
-                            var a_Context3 = Package.GetGlobalService(typeof(SVsShell)) as IVsShell;
-                            var a_Context4 = (object)null;
-                            if (a_Context3 != null)
-                            {
-                                a_Context3.GetProperty((int)__VSSPROPID7.VSSPROPID_MainWindowInfoBarHost, out a_Context4);
-                                a_Context2 = (IVsInfoBarHost)a_Context4;
-                            }
-                        }
-                        if (a_Context2 != null)
-                        {
-                            var a_Context3 = a_Context1.CreateInfoBar(new InfoBarModel(
-                                textSpans: new[]
-                                {
-                                    new InfoBarTextSpan(CONSTANT.NAME, true),
-                                    new InfoBarTextSpan(" extension doesn't work without "),
-                                    new InfoBarTextSpan("MetaOutput", true),
-                                    new InfoBarTextSpan("! Please install it.")
-                                },
-                                actionItems: new[]
-                                {
-                                    new InfoBarButton("Install MetaOutput")
-                                },
-                                image: KnownMonikers.StatusError));
-                            {
-                                a_Context3.Advise(new InfoBarService(), out s_Cookie);
-                                a_Context2.AddInfoBar(a_Context3);
-                            }
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Debug.WriteLine(ex.ToString());
-                }
-            }
-
-            public void OnClosed(IVsInfoBarUIElement infoBar)
-            {
-                try
-                {
-                    infoBar.Unadvise(s_Cookie);
-                }
-                catch (Exception ex)
-                {
-                    Debug.WriteLine(ex.ToString());
-                }
-            }
-
-            public void OnActionItemClicked(IVsInfoBarUIElement infoBar, IVsInfoBarActionItem action)
-            {
-                try
-                {
-                    Process.Start(atom.Trace.GetFailState(CONSTANT.APPLICATION));
-                }
-                catch (Exception ex)
-                {
-                    Debug.WriteLine(ex.ToString());
-                }
-            }
+            public const string VERSION = "1.0.10";
         }
 
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
@@ -113,8 +38,37 @@ namespace resource.package
             {
                 await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
             }
+            try
             {
-                InfoBarService.Validate();
+                if (string.IsNullOrEmpty(atom.Trace.GetFailState(CONSTANT.APPLICATION)) == false)
+                {
+                    var a_Context = Package.GetGlobalService(typeof(SDTE)) as DTE2;
+                    if (a_Context != null)
+                    {
+                        var a_Context1 = (OutputWindowPane)null;
+                        for (var i = a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Count; i >= 1; i--)
+                        {
+                            if (a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Item(i).Name == "MetaOutput")
+                            {
+                                a_Context1 = a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Item(i);
+                                break;
+                            }
+                        }
+                        if (a_Context1 == null)
+                        {
+                            a_Context1 = a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Add("MetaOutput");
+                        }
+                        if (a_Context1 != null)
+                        {
+                            a_Context1.OutputString("\r\n" + CONSTANT.NAME + " extension doesn't work without MetaOutput.\r\n    Please install it (https://www.metaoutput.net/download)\r\n");
+                            a_Context1.Activate();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
             }
         }
 

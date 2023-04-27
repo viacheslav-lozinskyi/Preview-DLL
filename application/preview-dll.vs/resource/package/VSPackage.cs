30ba42529d337d4274b9edd1b386c52c44521d84
Viacheslav Lozinskyi <viacheslav.lozinskyi@gmail.com>
2023-04-27 17:53:54 +0300

    - Minor changes

--- application/preview-dll.vs/resource/package/VSPackage.cs
+++ application/preview-dll.vs/resource/package/VSPackage.cs
@@ -20,6 +20,7 @@ namespace resource.package
         internal static class CONSTANT
         {
             public const string APPLICATION = "Visual Studio";
+            public const string COMPANY = "Viacheslav Lozinskyi";
             public const string COPYRIGHT = "Copyright (c) 2020-2023 by Viacheslav Lozinskyi. All rights reserved.";
             public const string DESCRIPTION = "Quick preview of DLL, EXE and SYS files";
             public const string GUID = "6171BB93-BA14-4ECF-B72B-5107D12E5D0F";
